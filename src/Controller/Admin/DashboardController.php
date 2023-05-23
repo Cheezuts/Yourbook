a7ba158dab2da29a0d124a38b0efec61376370b8
Cheezuts <telliercyril@gmail.com>
2023-05-23 17:12:30 +0200

    CRUD Auteur

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Auteur;
 use App\Entity\Editeur;
 use App\Entity\Genre;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
@@ -47,5 +48,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Genre', 'fas fa-list', Genre::class);
         yield MenuItem::linkToCrud('Editeur', 'fas fa-list', Editeur::class);
+        yield MenuItem::linkToCrud('Auteur', 'fas fa-list', Auteur::class);
     }
 }
