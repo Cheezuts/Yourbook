d154acf184480aa2dc292b583298fd8bd74c2e27
Cheezuts <telliercyril@gmail.com>
2023-05-24 10:54:57 +0200

    modif Entity Association avec Livre ( bug)

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Auteur;
 use App\Entity\Editeur;
 use App\Entity\Genre;
+use App\Entity\Livre;
 use App\Entity\Stock;
 use App\Entity\Usure;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
@@ -48,10 +49,11 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Genre', 'fas fa-list', Genre::class);
+        yield MenuItem::linkToCrud('Genre', 'fas fa-tags', Genre::class);
         yield MenuItem::linkToCrud('Editeur', 'fas fa-list', Editeur::class);
-        yield MenuItem::linkToCrud('Auteur', 'fas fa-list', Auteur::class);
+        yield MenuItem::linkToCrud('Auteur', 'fas fa-users', Auteur::class);
         yield MenuItem::linkToCrud('Usure', 'fas fa-list', Usure::class);
         yield MenuItem::linkToCrud('Stock', 'fas fa-list', Stock::class);
+        yield MenuItem::linkToCrud('Livre', 'fas fa-book', Livre::class);
     }
 }
