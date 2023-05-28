d75bd28415f2d85e84879eda11329a96db59007a
Cheezuts <telliercyril@gmail.com>
2023-05-28 18:18:23 +0200

    CRUD livre (front)

---
+++ src/Form/LivreType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Livre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('isbn')
+            ->add('description')
+            ->add('archive')
+            ->add('imageName')
+            ->add('imageSize')
+            ->add('updatedAt')
+            ->add('auteur')
+            ->add('editeur')
+            ->add('genres')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
